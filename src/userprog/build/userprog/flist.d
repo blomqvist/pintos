a6043671d72bffee6a5cae4ec2b369d2665f4b67
Niklas Blomqvist <nikbl918@astmatix.ida.liu.se>
2013-02-25 11:34:22 +0100

    fattar noll

---
+++ src/userprog/build/userprog/flist.d
@@ -0,0 +1,4 @@
+userprog/flist.o: ../../userprog/flist.c ../../lib/stdlib.h \
+  ../../lib/stddef.h ../../lib/stdio.h ../../lib/debug.h \
+  ../../lib/stdarg.h ../../lib/stdbool.h ../../lib/stdint.h \
+  ../../lib/kernel/stdio.h ../../userprog/flist.h
---
+++ src/userprog/build/userprog/sys_file_calls.d
@@ -0,0 +1,10 @@
+userprog/sys_file_calls.o: ../../userprog/sys_file_calls.c \
+  ../../lib/syscall-nr.h ../../lib/string.h ../../lib/stddef.h \
+  ../../lib/stdio.h ../../lib/debug.h ../../lib/stdarg.h \
+  ../../lib/stdbool.h ../../lib/stdint.h ../../lib/kernel/stdio.h \
+  ../../threads/interrupt.h ../../threads/thread.h \
+  ../../lib/kernel/list.h ../../userprog/flist.h ../../lib/stdlib.h \
+  ../../threads/init.h ../../threads/vaddr.h ../../threads/loader.h \
+  ../../filesys/filesys.h ../../filesys/off_t.h ../../filesys/file.h \
+  ../../devices/input.h ../../userprog/syscall.h ../../userprog/pagedir.h \
+  ../../userprog/process.h ../../userprog/sys_file_calls.h
