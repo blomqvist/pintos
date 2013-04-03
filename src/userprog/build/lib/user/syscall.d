707b086e7cc4d9f2f8452d575d07b4c935ea9d18
Niklas Blomqvist <niklas.blomqvist@gmail.com>
2013-04-03 10:32:38 +0200

    lol

--- src/userprog/build/lib/user/syscall.d
+++
@@ -1,2 +0,0 @@
-lib/user/syscall.o: ../../lib/user/syscall.c ../../lib/user/syscall.h \
-  ../../lib/stdbool.h ../../lib/debug.h ../../lib/syscall-nr.h
--- src/userprog/build/userprog/flist.d
+++
@@ -1,5 +0,0 @@
-userprog/flist.o: ../../userprog/flist.c ../../threads/malloc.h \
-  ../../lib/debug.h ../../lib/stddef.h ../../filesys/filesys.h \
-  ../../lib/stdbool.h ../../filesys/off_t.h ../../lib/stdint.h \
-  ../../lib/stdio.h ../../lib/stdarg.h ../../lib/kernel/stdio.h \
-  ../../userprog/flist.h ../../lib/stdlib.h
